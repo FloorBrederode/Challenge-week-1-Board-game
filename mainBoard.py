b510fefec5e630fffd2e5a854f3a9fe6a014b968
Floor <brederodef@gmail.com>
2024-03-04 11:09:50 +0100

    dice and start menu

--- mainBoard.py
+++ mainBoard.py
@@ -1,13 +1,32 @@
 import random
 
+
+# player location
 player_1 = 0
 player_2 = 0
+# which player is playing by doing math
+turn = 0
 
 # Dice
 def dice():
     roll = random.randint(1,6)
+    # add to player location
     return roll
 
 
+def end_game():
+    game == False
+
+
 if __name__ == '__main__':
-    print(dice())
+    print('Welcome to our bordgame')
+    user_start = input('Would you like to play? y/n').strip().lower()
+    if user_start == 'y':
+        ...
+    elif user_start == 'n':
+        ...
+    else:
+        print('Invalid input')
+    # roll = dice()
+    # print(roll)
+
