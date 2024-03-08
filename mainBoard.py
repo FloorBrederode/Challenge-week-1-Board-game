e1406822be2edbc553e1c605e1cec341072a8d33
Floor <brederodef@gmail.com>
2024-03-08 14:46:19 +0100

    shop in main board, mini problems tho

--- mainBoard.py
+++ mainBoard.py
@@ -1,5 +1,6 @@
 import random
 import choicequestions
+import shop
 import os
 
 number_question = 0
@@ -9,7 +10,7 @@ question_tiles = 0
 player_1_location = 1
 player_2_location = 1
 # starting scrap
-player_1_scrap = 0
+player_1_scrap = 100
 player_2_scrap = 0
 # make lists for both players so they can collect clothes
 player_1_list = []
@@ -17,6 +18,9 @@ player_2_list = []
 # which player is playing by doing math
 turn = 0
 current_player_turn = True
+# Types of tiles
+multiple_choice_tiles = [5, 21, 36, 48, 66, 76]
+shop_tiles = [10, 20, 30, 40, 50, 60, 70, 80, 90]
 
 
 # Dice
@@ -31,16 +35,70 @@ def move_player(player_location, outcome):
     return player_location
 
 def player_turn():
-    global player_1_location, player_2_location, turn
+    global turn, question_tiles
+    global player_1_location, player_1_scrap
     if turn % 2 == 0:
         print('Player 1')
         print(f"You rolled a {outcome}")
         player_1_location = move_player(player_1_location, outcome)
+        if player_1_location in multiple_choice_tiles:
+            question_tiles += 1
+            reward = choicequestions.questions(question_tiles)
+            player_1_scrap += reward
+            print(f'Player 1 now has {player_1_scrap} scrap.')
+
+        elif player_1_location in shop_tiles:
+
+            while True:
+                store_answer = input(f'''You land on a store Tile.
+            You have {player_1_scrap} scrap.
+            Do you enter the store? y/n ''').lower().strip()
+
+                if store_answer == 'y':
+                    items_bought = shop.shop(player_1_scrap)
+                    print(items_bought)
+                    player_1_scrap = int(items_bought[0])
+                    print(player_1_scrap)
+                    player_1_list.append(items_bought[1])
+                    print(player_1_list)
+                    break
+                elif store_answer == 'n':
+                    break
+                else:
+                    print('Invalid input')
+
+    global player_2_list, player_2_location, player_2_scrap
 
     if turn % 2 == 1:
         print('Player 2')
         print(f"You rolled a {outcome}")
         player_2_location = move_player(player_2_location, outcome)
+        
+        if player_2_location in multiple_choice_tiles:
+            question_tiles += 1
+            reward = choicequestions.questions(question_tiles)
+            player_2_scrap += reward
+            print(f'Player 2 now has {player_2_scrap} scrap.')
+
+    elif player_1_location in shop_tiles:
+
+            while True:
+                store_answer = input(f'''You land on a store Tile.
+                You have {player_1_scrap} scrap.
+                Do you enter the store? y/n ''').lower().strip()
+
+                if store_answer == 'y':
+                    items_bought = shop.shop(player_1_scrap)
+                    print(items_bought)
+                    player_1_scrap = int(items_bought[0])
+                    print(player_1_scrap)
+                    player_1_list.append(items_bought[1])
+                    print(player_1_list)
+                    break
+                elif store_answer == 'n':
+                    break
+                else:
+                    print('Invalid input')
 
 
 if __name__ == '__main__':
@@ -55,17 +113,8 @@ if __name__ == '__main__':
             roll_prompt = input("Press Enter to roll your dice!")
             outcome = dice()
             os.system('cls')
-            player_turn()          
-            if player_1_location in [5, 21, 36, 48, 66, 76, ]:
-                question_tiles += 1
-                reward = choicequestions.questions(question_tiles)
-                player_1_scrap += reward
-                print(f'You now have {player_1_scrap} scrap.')
-            if player_2_location in [5, 21, 36, 48, 66, 76, ]:
-                question_tiles += 1
-                reward = choicequestions.questions(question_tiles)
-                player_1_scrap += reward
-                print(f'You now have {player_1_scrap} scrap.')
+            player_turn()  
+
             print(f'Player 1 location: Tile {player_1_location}')  
             print(f'Player 2 location: Tile {player_2_location}') 
             turn += 1            
--- shop.py
+++ shop.py
@@ -8,18 +8,13 @@
 
 def shop(player_scrap):
     scrap = player_scrap
-    print('Welcome to the shop!')
-    print(f'You have {scrap} scrap')
-    shop_answer = input('Would you like to buy something? y/n ').lower().strip()
 
     while True:
-        if shop_answer == 'y':
             print('''We have,
             - [A] Hats...(10 Scrap)
             - [B] Shirts...(15 Scrap)
             - [C] Pants...(20 Scrap)
             - [D] Shoes...(30 Scrap)
-            - [E] An extra Roll...(50 Scrap)
             [L] Leave''')
 
             player_buying = input('Is there something you want? ')
@@ -28,62 +23,49 @@ def shop(player_scrap):
                 if scrap < 10:
                     print('You don\'t have enough scrap to buy this')
                     input()
-                elif scrap > 10:
+                else:
                     print('Here\'s your new hat.')
                     scrap -= 10
-                    print(scrap)
                     input()
+                    return (scrap, 'hat')
+                    # it needs to return the amount of scrap now had and an item
             elif player_buying == 'b':
                 if scrap < 15:
                     print('You don\'t have enough scrap to buy this')
                     input()
-                elif scrap > 15:
-                    print('Here\'s your new hat.')
+                else:
+                    print('Here\'s your new shirt.')
                     scrap -= 15
-                    print(scrap)
                     input()
+                    return (scrap, 'shirt')
                 break
             elif player_buying == 'c':
                 if scrap < 20:
                     print('You don\'t have enough scrap to buy this')
                     input()
-                elif scrap > 20:
-                    print('Here\'s your new hat.')
+                else:
+                    print('Here\'s your new pants.')
                     scrap -= 20
-                    print(scrap)
                     input()
+                    return (scrap, 'pants')
                 break
             elif player_buying == 'd':
                 if scrap < 30:
                     print('You don\'t have enough scrap to buy this')
                     input()
-                elif scrap > 30:
-                    print('Here\'s your new hat.')
+                else:
+                    print('Here are your new shoes.')
                     scrap -= 30
-                    print(scrap)
                     input()
-                break
-            elif player_buying == 'e':
-                if scrap < 50:
-                    print('You don\'t have enough scrap to buy this')
-                    input()
-                elif scrap > 50:
-                    print('Here\'s your new hat.')
-                    scrap -= 50
-                    print(scrap)
-                    input()
+                    return (scrap, 'shoes')
                 break
             elif player_buying == 'l':
                 print('Goodbye')
                 break
             else:
                 print('We dont sell that.')
-        elif shop_answer == 'n':
             print('Goodbye!')
             break
-        else:
-            print('Invalid input')
 
 
 player_1_scrap = 15
-shop(player_1_scrap)
