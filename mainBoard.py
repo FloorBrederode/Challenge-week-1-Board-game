613e484815c288105c314d4e1b2acd1931c3cda5
Floor <brederodef@gmail.com>
2024-03-08 19:55:00 +0100

    snakes

--- mainBoard.py
+++ mainBoard.py
@@ -6,9 +6,7 @@ import os
 number_question = 0
 
 question_tiles = 0
-# player location
-player_1_location = 1
-player_2_location = 1
+
 # starting scrap
 player_1_scrap = 100
 player_2_scrap = 0
@@ -36,17 +34,22 @@ def move_player(player_location, outcome):
 
 def player_turn():
     global turn, question_tiles
+
+    # player 1
     global player_1_location, player_1_scrap
     if turn % 2 == 0:
         print('Player 1')
         print(f"You rolled a {outcome}")
         player_1_location = move_player(player_1_location, outcome)
+
+        # multiple choice questions 
         if player_1_location in multiple_choice_tiles:
             question_tiles += 1
             reward = choicequestions.questions(question_tiles)
             player_1_scrap += reward
             print(f'Player 1 now has {player_1_scrap} scrap.')
 
+        # shop
         elif player_1_location in shop_tiles:
 
             while True:
@@ -67,62 +70,122 @@ def player_turn():
                 else:
                     print('Invalid input')
 
+        # snakes
+        elif player_1_location == 45:
+            print('''
+            OH NO.
+            You landed on a snake tile.
+            You go back to tile 27.
+            ''')
+            player_1_location = 27
+        elif player_1_location == 68:
+            print('''
+            OH NO.
+            You landed on a snake tile.
+            You go back to tile 54.
+            ''')
+            player_1_location = 54
+        elif player_1_location == 96:
+            print('''
+            OH NO.
+            You landed on a snake tile.
+            You go back to tile 75.
+            ''')
+            player_1_location = 75
+
+    # player 2
     global player_2_list, player_2_location, player_2_scrap
 
     if turn % 2 == 1:
         print('Player 2')
         print(f"You rolled a {outcome}")
         player_2_location = move_player(player_2_location, outcome)
-        
+
+        # multiple choice questions
         if player_2_location in multiple_choice_tiles:
             question_tiles += 1
             reward = choicequestions.questions(question_tiles)
             player_2_scrap += reward
             print(f'Player 2 now has {player_2_scrap} scrap.')
 
-    elif player_1_location in shop_tiles:
+        elif player_2_location in shop_tiles:
 
+            # shop
             while True:
                 store_answer = input(f'''You land on a store Tile.
-                You have {player_1_scrap} scrap.
+                You have {player_2_scrap} scrap.
                 Do you enter the store? y/n ''').lower().strip()
 
                 if store_answer == 'y':
-                    items_bought = shop.shop(player_1_scrap)
+                    items_bought = shop.shop(player_2_scrap)
                     print(items_bought)
-                    player_1_scrap = int(items_bought[0])
-                    print(player_1_scrap)
-                    player_1_list.append(items_bought[1])
-                    print(player_1_list)
+                    player_2_scrap = int(items_bought[0])
+                    print(player_2_scrap)
+                    player_2_list.append(items_bought[1])
+                    print(player_2_list)
                     break
                 elif store_answer == 'n':
                     break
                 else:
                     print('Invalid input')
 
+        # snakes
+        elif player_2_location == 45:
+            print('''
+            OH NO.
+            You landed on a snake tile.
+            You go back to tile 27.
+            ''')
+            player_2_location = 27
+        elif player_2_location == 68:
+            print('''
+            OH NO.
+            You landed on a snake tile.
+            You go back to tile 54.
+            ''')
+            player_2_location = 54
+        elif player_2_location == 96:
+            print('''
+            OH NO.
+            You landed on a snake tile.
+            You go back to tile 75.
+            ''')
+            player_2_location = 75
+        
+
 
 if __name__ == '__main__':
     print('Welcome to our bordgame')
-    user_start = input('Would you like to play? y/n ').strip().lower()
-    if user_start == 'y':
+    print('''The theme of this boardgame is a nuclear distopian.
+    The main objective is to get to tile 100 first.
+    There is a side objective, by collecting a complete outfit you get an extra reward.''')
 
-        while True:
-            if not current_player_turn:
-                print("Next Player's turn!")
-            current_player_turn = False
-            roll_prompt = input("Press Enter to roll your dice!")
-            outcome = dice()
-            os.system('cls')
-            player_turn()  
+    while True:    
+        user_start = input('Would you like to play? y/n ').strip().lower()
+        if user_start == 'y':
 
-            print(f'Player 1 location: Tile {player_1_location}')  
-            print(f'Player 2 location: Tile {player_2_location}') 
-            turn += 1            
-            continue                     
-    elif user_start == 'n':
-        print('Goodbye')
-    else:
-        print('Invalid input')
+            # player location
+            player_1_location = 1
+            player_2_location = 1
+
+            while player_1_location < 100 and player_2_location < 100:
+                if not current_player_turn:
+                    print("Next Player's turn!")
+                current_player_turn = False
+                roll_prompt = input("Press Enter to roll your dice!")
+                outcome = dice()
+                os.system('cls')
+                player_turn()  
+
+                print(f'Player 1 location: Tile {player_1_location}')  
+                print(f'Player 2 location: Tile {player_2_location}') 
+                turn += 1            
+                continue                     
+        elif user_start == 'n':
+            print('Goodbye')
+            break
+        else:
+            print('Invalid input')
         
     
 
