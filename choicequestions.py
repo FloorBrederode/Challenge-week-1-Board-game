ae685b57bea57811fdbfbd312fa7d3560b54006b
Floor <brederodef@gmail.com>
2024-03-08 20:19:48 +0100

    added a choice question, fixed small things

--- choicequestions.py
+++ choicequestions.py
@@ -8,10 +8,10 @@ def questions(number_question):
     while True:
         # How long no food
         if choice_question == 1:
-            print('How long can a person go without food?')
-            print('A: 8 to 21 Days.')
-            print('B: 21 to 36 Days.')
-            print('C: 36 to 45 Days.')
+            print('''How long can a person go without food?
+            A: 8 to 21 Days.
+            B: 21 to 36 Days.
+            C: 36 to 45 Days.''')
             answer_choice = input('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('''Correct!
@@ -38,10 +38,10 @@ def questions(number_question):
 
         # Who invented nuclear bomb
         elif choice_question == 2:
-            print('Who invented the nuclear bomb')
-            print('A: Lea Szilard.')
-            print('B: J. Robert Oppenheimer.')
-            print('C: Cillian Murphy.')
+            print('''Who invented the nuclear bomb?
+            A: Lea Szilard.
+            B: J. Robert Oppenheimer.
+            C: Cillian Murphy.''')
             answer_choice = input('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('Incorrect')
@@ -68,13 +68,13 @@ def questions(number_question):
 
         # mafths
         elif choice_question == 3:
-            print('The radiation level in a certain area dubbles every hour.')
-            print('If the radiation level is initially at 10 millisieverts per hour (mSv/h),')
-            print('what will be the radiation level after 3 hours?')
-            print('A: 30 millisieverts per hour (mSv/h)')
-            print('B: 40 millisieverts per hour (mSv/h)')
-            print('C: 60 millisieverts per hour (mSv/h)')
-            print('D: 80 millisieverts per hour (mSv/h)')
+            print('''The radiation level in a certain area dubbles every hour.')
+            If the radiation level is initially at 10 millisieverts per hour (mSv/h),
+            what will be the radiation level after 3 hours?
+            A: 30 millisieverts per hour (mSv/h)
+            B: 40 millisieverts per hour (mSv/h)
+            C: 60 millisieverts per hour (mSv/h)
+            D: 80 millisieverts per hour (mSv/h)''')
             answer_choice = input ('A, B, C, or D? ').lower().strip()
             if answer_choice == 'a':
                 print('Incorrect')
@@ -108,10 +108,10 @@ def questions(number_question):
 
         # how much radiation?
         elif choice_question == 4:
-            print('What is the minimum amout of radiation that will kill you?')
-            print('A: A dose of 100 mSv. (millisievert)')
-            print('B: A dose of 1000 mSv. (millisievert)')
-            print('C: A dose of 10000 mSv. (millisievert)')
+            print('''What is the minimum amout of radiation that will kill you?
+            A: A dose of 100 mSv. (millisievert)
+            B: A dose of 1000 mSv. (millisievert)
+            C: A dose of 10000 mSv. (millisievert)''')
             answer_choice = input ('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('Incorrect')
@@ -140,10 +140,10 @@ def questions(number_question):
 
         # What is the reccomended material for shielnidn against radiation exposure
         elif choice_question == 5:
-            print('What is the recommended material for shielding agains radiation exposure?')
-            print('A: Lead.')
-            print('B: Aluminium.')
-            print('C: Steel.')
+            print('''What is the recommended material for shielding agains radiation exposure?
+            A: Lead.
+            B: Aluminium.
+            C: Steel.''')
             answer_choice = input ('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('''Correct
@@ -171,10 +171,10 @@ def questions(number_question):
 
         #what is not a symptom id acute radiation
         elif choice_question == 6:
-            print('What is not a symptom of acute Radiation Sickness?')
-            print('A: Hairloss.')
-            print('B: Nausea and Vomiting.')
-            print('C: Blindness.')
+            print('''What is not a symptom of acute Radiation Sickness?')
+            A: Hairloss.
+            B: Nausea and Vomiting.
+            C: Blindness.''')
             answer_choice = input ('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('Incorrect')
@@ -203,10 +203,11 @@ def questions(number_question):
 
         #what type of clothing provides better protecton agains radiation
         elif choice_question == 7:
-            print('What type of clothing provides the best protection agains radiation?')
-            print('A: Thick Wool.')
-            print('B: Synthetic Fibers.')
-            print('C: Lead-Lined Suits.')
+            print('''
+            What type of clothing provides the best protection agains radiation?
+            A: Thick Wool.
+            B: Synthetic Fibers.
+            C: Lead-Lined Suits.''')
             answer_choice = input ('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('Incorrect')
@@ -234,9 +235,11 @@ def questions(number_question):
 
         #which food item is generaly considers safer to consume during a radiation emergency
         elif choice_question == 8:
-            print('A: Fresh Produce.')
-            print('B: Canned Goods.')
-            print('C: Wild Game.')
+            print('''
+            Which food item is generaly considers safest to consume during a radiation emergency?
+            A: Fresh Produce.
+            B: Canned Goods.
+            C: Wild Game.''')
             answer_choice = input ('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('Incorrect')
@@ -264,9 +267,11 @@ def questions(number_question):
 
         # which factor contributes to the spread of radiation contamination
         elif choice_question == 9:
-            print('A: Wind Direction.')
-            print('B: Humidity Level.')
-            print('C: Lunar Phase.')
+            print('''
+            Which factor contrubutes to the spread of radiation contamination?
+            A: Wind Direction.
+            B: Humidity Level.
+            C: Lunar Phase. ''')
             answer_choice = input ('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('''Correct
@@ -294,9 +299,11 @@ def questions(number_question):
 
         #what type od water source is asafer to consume in a radiated area
         elif choice_question == 10:
-            print('A: Running Water.')
-            print('B: Rainwater collected in Barrels')
-            print('C: Water in Standing Ponds.')
+            print('''
+            What type of water source is safest to consume in a radiated area?
+            A: Running Water.
+            B: Rainwater collected in Barrels
+            C: Water in Standing Ponds.''')
             answer_choice = input ('A, B, or C? ').lower().strip()
             if answer_choice == 'a':
                 print('''Correct
@@ -320,4 +327,45 @@ def questions(number_question):
             else:
                 print('Invalid Input')
                 input()
+
+        # which element is commonly used as fuel in nuclear reactors?
+        elif choice_question == 11:
+            print('''
+            Which element is commonly used as fuel in nuclear reactors?
+            A: Uranium
+            B: Carbon
+            C: Oxygen
+            D: Hydrogen''')
+            answer_choice = input ('A, B, C, or D? ').lower().strip()
+            if answer_choice == 'a':
+                print('''Correct
+                You recieve 15 scrap.''')
+                choice_question += 1
+                reward = 15
+                return reward
+                break
+            elif answer_choice == 'b':
+                print('Incorrect')
+                choice_question += 1
+                reward = 0
+                return reward
+                break
+            elif answer_choice == 'c':
+                print('Incorrect')
+                choice_question += 1
+                reward = 0
+                return reward
+                break
+            elif answer_choice == 'd':
+                print('Incorrect')
+                choice_question += 1
+                return reward
+                break
+            else:
+                print('Invalid Input')
+                input()
+
+        if choice_question == 11:
+            choice_question = 0
+
 
